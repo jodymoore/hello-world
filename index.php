266e5f78bcd07efcb12caa99354b950b166c14eb
Jody Moore <jodymoore@g.harvard.edu>
2017-02-04 13:56:37 -0600

    initial commit index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<html>
+  <head>
+
+	  <title>Jody Moore</title>
+	  <meta charset="utf-8">
+
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	  <link rel="stylesheet" href="" type="text/css">
+	  <link href="" rel="stylesheet">
+       <style type="text/css">
+  
+		#div1 img {
+			display: block;
+		    margin-left: auto;
+		    margin-right: auto;
+		}
+		#div1 h2{
+		  text-align: center;
+		}
+		#div1 h1{
+		  text-align: center;
+		}
+		#div1 blockquote{
+		  text-align: center;
+		}
+		#div1 p{
+		  display: inline-block;
+          margin: 200px 0 0 73px;
+		}
+
+	   </style>
+
+	  <link href="/css/styles.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <div id="div1"> 
+		  <h1>Jody Moore</h1>
+
+		  <img id="pic" src="img/myPic2.jpg" alt="Jody Moore">
+
+		  <h2>About Me</h2>
+		  <p>			
+          Lorem ipsum dolor sit amet, sociosqu donec varius orci convallis enim, enim enim tempor mollis, et maecenas arcu fermentum, faucibus rutrum, turpis consequat nec laoreet libero. Pellentesque mauris penatibus viverra sed erat, pede sed ante, vel nulla lacus a. Odio eu, tristique adipiscing lacus malesuada mauris, lectus mauris hendrerit aenean.
+		  </p>
+		  <h2>Random Quote</h2>
+		  <blockquote>
+		  	Do what you feel in your heart to be right, for you’ll be criticized anyway. -Eleanor Roosevelt		</blockquote>
+	</div>
+
+	  <script src=""></script>
+
+  </body>
+</html>
